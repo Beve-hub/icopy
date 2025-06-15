f31f87ee9edb109332caafb78bf7d9f9528686bb
Believe <117173080+Beve-hub@users.noreply.github.com>
2025-06-15 20:03:22 +0100

    update

--- utils/card-component.js
+++ utils/card-component.js
@@ -3,15 +3,15 @@ cardData.forEach(card => {
     const cardElement = document.createElement('div');
     cardElement.className = 'col-md-4';
     cardElement.innerHTML = `
-        <div class="card m-3 position-relative" style="width: 22rem; background-color: var(--color-back); color: white;  cursor:pointer; ">
+        <div class="card m-3 position-relative" style="width: auto; background-color: var(--color-back); color: white;  cursor:pointer; ">
             <div class="card-body">
                 <span class="badge position-absolute top-0 start-0 m-2 " style="background-color: #D4FF27; color: #121212; transform: rotate(90deg); transform-origin: top left;">${card.pack}</span>
                 <h5 class="card-title mt-4">${card.title}</h5>
                 <h2 class="card-subtitle mb-2 fs-2" style="color: #D4FF27;">${card.amount}</h2>
                 <ul class="list-group list-group-flush" style="background-color: transparent;">
-                   ${card.offers.map(offer => `<li class="list-group-item" style="background-color: transparent; color: white;"><i class="bi bi-check" style="color: green;"></i> ${offer}</li>`).join('')} 
+                   ${card.offers.map(offer => `<li class="list-group-item " style="background-color: transparent; color: white; font-size: 0.8rem;"><i class="bi bi-check" style="color: green;"></i> ${offer}</li>`).join('')} 
                 </ul>
-                <button style="background-color: #D4FF27; color: #121212; width:18rem; margin:10px 0 ">Fund</button>
+                <button style="background-color: #D4FF27; color: #121212;  margin:10px 0; display:flex; align-items:center; justify-content:center; width: 100%;">Fund</button>
             </div>
         </div>
     `;

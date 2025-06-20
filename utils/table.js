61b510b9583c6b9744c48ba20d50723a27b19243
Believe <117173080+Beve-hub@users.noreply.github.com>
2025-06-20 18:38:48 +0100

    update

--- utils/table.js
+++ utils/table.js
@@ -1,31 +1,36 @@
 class DepositTable extends HTMLElement {
   connectedCallback() {
+    const width = this.getAttribute('width') || '100%';
+
     this.innerHTML = `
-      <table class="table table-sm">
-        <thead>
-          <tr>
-            <th scope="col"># </th>
-            <th scope="col">Order ID</th>
-            <th scope="col">Account</th>
-            <th scope="col">Amount</th>
-            <th scope="col">Method</th>
-            <th scope="col">Status</th>
-            <th scope="col">Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row"></th>
-            <td>lorem</td>
-            <td>ipsum</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
+      <div style="width: ${width}; overflow-x: auto;">
+        <table class="table table-sm" style="width: 100%;">
+          <thead>
+            <tr>
+              <th scope="col" style=" font-size:13px;"># </th>
+              <th scope="col" style=" font-size:13px;">Order ID</th>
+              <th scope="col" style=" font-size:13px;">Account</th>
+              <th scope="col" style=" font-size:13px;">Amount</th>
+              <th scope="col" style=" font-size:13px;">Method</th>
+              <th scope="col" style=" font-size:13px;">Status</th>
+              <th scope="col" style=" font-size:13px;">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row"></th>
+              <td >lorem</td>
+              <td >ipsum</td>
+              <td ></td>
+              <td ></td>
+              <td ></td>
+              <td ></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     `;
   }
 }
+
 customElements.define('deposit-table', DepositTable);
